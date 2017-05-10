11f2cd00094be039a9030fb9d03f99beea013b0d
Jared D. Baker <jbaksta@gmail.com>
2017-05-10 10:15:18 -0600

    added packages and maybe repos

--- etc/puppet/modules/base/manifests/init.pp
+++ etc/puppet/modules/base/manifests/init.pp
@@ -40,13 +40,44 @@ file { "/etc/profile.d/z01_modules.csh":
 	source => "puppet:///modules/base/z01_modules.csh",
 }
 
+yumrepo { "RHoptional":
+	name => "rhui-REGION-rhel-server-optional",
+	ensure => "present",
+	enabled => "yes"
+}
+
+yumrepo { "RHextras":
+	name => "rhui-REGION-rhel-server-extras",
+	ensure => "present",
+	enabled => "yes",
+}
+
 
 Package { ensure => 'installed' }
 
-$pkgs = [ 'bash-completion', 'readline', 'readline-devel', 'libgenders', 'libgenders-devel', 'lua',
-          'lua-devel', 'lua-lpeg', 'lua-filesystem', 'lua-posix', 'lua-json', 'lua-term', 'nfs-utils',
-	  'tcl', 'tcl-devel', 'tk', 'tk-devel', 'libical-devel', 'rpm-build', 'hwloc-devel', 'libXt-devel',
-          'libedit-devel', 'postgresql-devel', 'python-devel', 'openssl-devel', 'libXext', 'wget' ]
+$pkgs = [ 'bash-completion','vim',
+          'readline', 'readline-devel', 
+          'libgenders', 'libgenders-devel',
+          'lua','lua-devel', 'lua-lpeg', 'lua-filesystem', 'lua-posix', 'lua-json', 'lua-term', 
+          'nfs-utils',
+	  'tcl', 'tcl-devel', 
+          'tk', 'tk-devel', 
+          'libical', 'libical-devel', 
+          'rpm-build', 
+          'hwloc', 'hwloc-devel',
+          'libXt-devel',
+          'libedit-devel',
+          'python','python-devel',
+          'wget','curl','libcurl',
+          'gcc','gcc-gfortran','gcc-c++','make','autoconf','automake'
+          'libX11-devel','libXext','libXt-devel','libXft-devel',
+          'libedit-devel','ncurses-devel',
+          'perl',
+	  'postgresql-server','postgresql-devel',
+          'expat','expat-devel',
+          'openssl','openssl-devel',
+          'fontconfig',
+        ]
 
 package { $pkgs: }
 

08eaec697440bad57a1326010a31f17b595d2597
Jared D. Baker <jbaksta@gmail.com>
2017-05-12 16:13:54 -0600

    other auth files

--- etc/puppet/modules/users/manifests/init.pp
+++ etc/puppet/modules/users/manifests/init.pp
@@ -9,5 +9,22 @@ file { "/etc/passwd":
 	source => "puppet:///modules/users/passwd",
 }
 
+file { "/etc/shadow":
+	ensure => "file",
+	owner => "root",
+	group => "root",
+	mode => "644",
+	links => "follow",
+	source => "puppet:///modules/users/shadow",
+}
+
+file { "/etc/group":
+	ensure => "file",
+	owner => "root",
+	group => "root",
+	mode => "644",
+	links => "follow",
+	source => "puppet:///modules/users/group",
+}
 
 }

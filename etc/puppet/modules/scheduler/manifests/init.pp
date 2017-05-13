93f44ac0a6dc0699c89a68ad68a4fab5747a8b7f
Jared D. Baker <jbaksta@gmail.com>
2017-05-13 17:54:57 -0600

    puppet cleanup

--- etc/puppet/modules/scheduler/manifests/init.pp
+++ etc/puppet/modules/scheduler/manifests/init.pp
@@ -1,19 +1,3 @@
 class scheduler {
 
-        package {"pbspro-execution":
-                ensure => "present",
-        }
-
-        file { "/etc/pbs.conf":
-                ensure => "file",
-                owner => "root",
-                group => "root",
-                mode => "0644",
-                source => "puppet:///modules/clients/pbs.conf",
-        }
-
-        service {"pbs":
-                ensure => "running",
-                enable => "true",
-        }
 }

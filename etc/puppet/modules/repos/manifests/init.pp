ddf154da5ac18f33e2ff7a5244873d17811b697f
Jared D. Baker <jbaksta@gmail.com>
2017-05-13 17:27:09 -0600

    more puppet stuff

---
+++ etc/puppet/modules/repos/manifests/init.pp
@@ -0,0 +1,32 @@
+class repos {
+
+	# Make sure we have all the necessary YUM repos
+	yumrepo { "RHoptional":
+		name => "rhui-REGION-rhel-server-optional",
+		enabled => "yes"
+	}
+
+	yumrepo { "RHextras":
+		  name => "rhui-REGION-rhel-server-extras",
+		  enabled => "yes",
+	}
+
+	package {"epel-release":
+		ensure => "present",
+		source => "https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+	}
+
+	yumrepo { "EPEL":
+		name => "epel",
+		enabled => "yes"
+	}
+
+	yumrepo { "LCI-BAC":
+		name => "hpc",
+		descr => "LCI Build-A-Cluster",
+		baseurl => "http://master/hpc-repo",
+		enabled => "yes",
+		gpgcheck => "no",
+	}
+
+}

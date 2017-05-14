4a560e405326eadef318ecab796b91a39136b2b8
Jared D. Baker <jbaksta@gmail.com>
2017-05-13 20:20:32 -0600

    started build script for pbs

---
+++ scripts/pbspro_build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Script to build and install PBS Pro
+#
+
+# Clone the open source version of PBS Pro
+git clone https://github.com/PBSPro/pbspro.git
+
+cd pbspro
+
+# Check out a specific release for a known good condition
+git checkout release_to_be_determined #TODO 
+
+# Not sure why this is here
+git reset --hard
+git clean -f -d -x -f
+
+# Generate the configure script
+bash ./autogen.sh
+
+# Configure the installation
+./configure --prefix=/opt/pbs --libexecdir=/opt/pbs/libexec
+
+# Build the binaries and generate a compressed tarballs
+make -j4 dist
+
+# Copy all tarballs to the appropriate rpmbuild directory
+mkdir -p ~/rpmbuild/SOURCES
+cp -fv pbspro-*.tar.gz ~/rpmbuild/SOURCES/
+
+# Build the RPMs from the tarballs
+rpmbuild -bb pbspro.spec
+
+# Add the RPMs to the HPC repo
+sudo cp ~/rpmbuild/RPMS/x86_64/pbs*rpm /var/www/html/hpc-repo/Packages
+
+# Update the YUM repo
+cd /var/www/html/hpc-repo
+sudo createrepo $(pwd)
+
+# Update the YUM cache on all nodes
+# sudo clush -w @compute yum makecache fast
+
+# Install the PBS Pro Server on master node
+# sudo yum install pbspro-server
+
+# Install the PBS Pro Execution package on compute nodes
+# (This should be done in Puppet technically)
+# sudo clush -w @compute yum install -y pbspro-execution
+
+# Edit the /etc/pbs.conf for master server
+
+# Edit the /etc/pbs.conf for compute nodes
+
+# Start the PBS server on the master
+
+# Add the compute nodes to the server configuration
+
+# Start the PBS MOMs on the compute nodes
+
+# Make some optimizations about shared filesystems
+
+# Next -> Build OpenMPI
+
+
+
+
